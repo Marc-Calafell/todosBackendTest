80532a39a48cd0c2743edb7d59a86f86617cf787
Marc <mcalafellsmx@gmail.com>
2016-11-29 19:24:38 +0100

    Initial version

---
+++ tests/TaskControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TaskControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     * @group todo
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
