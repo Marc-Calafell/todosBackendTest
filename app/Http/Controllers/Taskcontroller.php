adf7ca197d126f867242c89df25d86a76c470e46
Marc <mcalafellsmx@gmail.com>
2016-11-29 21:30:33 +0100

    added task using todosBackend API

--- app/Http/Controllers/Taskcontroller.php
+++ app/Http/Controllers/Taskcontroller.php
@@ -3,12 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
+/**
+ * Class Taskcontroller
+ * @package App\Http\Controllers
+ */
 class Taskcontroller extends Controller {
 
-    public function task() {
-        return view('tasks')->with('tasks',[ ]);
+    /**
+     * @var Client
+     */
+    protected $client;
 
+    /**
+     * @var string
+     */
+    protected $urlTodosBackend='http://localhost:8000/api/v1/task';
+
+    /**
+     * Taskcontroller constructor.
+     * @param Client $client
+     */
+    public function __construct(Client $client) {
+        $this->client = $client;
+    }
+
+    /**
+     * @return $this
+     */
+    public function task() {
+        $tasks = \GuzzleHttp\json_decode($this->client->request('GET',$this->urlTodosBackend)->getBody())->data;
+//        dd($tasks);
+        return view('tasks')->with('tasks', $tasks);
     }
 
 
