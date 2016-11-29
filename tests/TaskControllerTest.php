0b95432f73b8849ab3cda6ab90c5fa4a7a8e2f74
Marc <mcalafellsmx@gmail.com>
2016-11-29 20:17:51 +0100

    added create aplication method

--- tests/TaskControllerTest.php
+++ tests/TaskControllerTest.php
@@ -4,18 +4,41 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class TaskControllerTest extends TestCase
-{
+
+
+class TaskControllerTest extends TestCase {
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      * @group todo
      * @return void
      */
-    public function testExample()
-    {
+    public function testExample()  {
+
+        $this->login();
+
+
         //$this->assertTrue(true);
         $response = $this -> call('GET','/tasques');
         $this->assertEquals(200,$response->status());
         $this->assertViewHas('tasks');
+    }
+
+    protected function login(){
+        $user = factory(\App\User::class)->create();
+        $this->actingAs($user);
+
+    }
+
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
     }
 }
